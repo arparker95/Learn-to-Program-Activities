d6bdfe9fc872eaa1d641ae44f4d80390010c44fc
arparker95 <31907242+arparker95@users.noreply.github.com>
2017-10-09 21:50:08 -0700

    Add files via upload

---
+++ activity10.6.py
@@ -0,0 +1,18 @@
+age = 21
+name = "Alexis Parker"
+gpa = "unknown"
+
+print "I am {} years old".format(age)
+print "My name is {}".format(name)
+print "In school my GPA was {}".format(gpa)
+
+operand1 = 978.425
+operand2 = 0.0324
+
+print "{} + {} = {}".format(operand1, operand2, operand1 + operand2)
+print "{} - {} = {}".format(operand1, operand2, operand1 - operand2)
+print "{} * {} = {}".format(operand1, operand2, operand1 * operand2)
+print "{} / {} = {}".format(operand1, operand2, operand1 / operand2)
+
+print "{} % {} = {}".format(9,3,9 % 3)
+print "{} % {} = {}".format(112,10,112 % 10)
