4c552ffc816ec91283d6bf291f1364946797bb83
arparker95 <31907242+arparker95@users.noreply.github.com>
2017-10-14 18:48:26 -0700

    Add files via upload

---
+++ ifStatements.py
@@ -0,0 +1,48 @@
+import random
+question = raw_input("What is your question? ")
+answer = random.randint(1,20)
+print "Question: {}".format(question)
+if answer == 1:
+    print "No."
+elif answer == 2:
+    print "Absolutely not."
+elif answer == 3:
+    print "Sure, why not."
+elif answer == 4:
+    print "I'm not sure why you're asking this, but no."
+elif answer == 5:
+    print "Yes!"
+elif answer == 6:
+    print "I can't answer that."
+elif answer == 7:
+    print "I'm pretty sure you could look this up and find the answer yourself."
+elif answer == 8:
+    print "Good question, but no."
+elif answer == 9:
+    print "The answer is yes."
+elif answer == 10:
+    print "Nope."
+elif answer == 11:
+    print "Sure."
+elif answer == 12:
+    print "Absolutely!"
+elif answer == 13:
+    print "No, I don't think so."
+elif answer == 14:
+    print "It's possible."
+elif answer == 15:
+    print "Probably."
+elif answer == 16:
+    print "Probably not."
+elif answer == 17:
+    print "Definitely not."
+elif answer == 18:
+    print "Yes, for sure."
+elif answer == 19:
+    print "Yes definitely!"
+elif answer == 20:
+    print "Yes."
+    
+    
+    
+    
